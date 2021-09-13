e37332807568e73b6bcf0da410ac85b6577f3768
mazurasdf <mazurasdf@gmail.com>
2021-09-13 16:24:09 -0500

    added routes and controller functions for CRUD

--- backEnd/mongooseIntro/server/controllers/iceCream.controller.js
+++ backEnd/mongooseIntro/server/controllers/iceCream.controller.js
@@ -1,5 +1,42 @@
 const IceCream = require("../models/iceCream.model");
 
 module.exports.findAllIceCreams = (req,res) => {
-    res.json({message:"hey it's me!"});
+    console.log("hey it's me, the find all function!");
+
+    IceCream.find({})
+        .then(allIceCreams => res.json({results: allIceCreams}))
+        .catch(err => res.json({message: "that didn't quite work", err}))
+}
+
+module.exports.createIceCream = (req,res) => {
+    IceCream.create(req.body)
+        .then(newIceCream => res.json({results: newIceCream}))
+        .catch(err => res.json({message: "that didn't quite work", err}))
+}
+
+module.exports.findSingleIceCream = (req,res) => {
+    IceCream.findOne({_id: req.params._id})
+        .then(singleIceCream => res.json({results: singleIceCream}))
+        .catch(err => res.json({message: "that didn't quite work", err}))
+}
+
+module.exports.deleteSingleIceCream = (req,res) => {
+    IceCream.deleteOne({_id: req.params._id})
+        .then(results => res.json({results: results}))
+        .catch(err => res.json({message: "that didn't quite work", err}))
+}
+
+module.exports.updateSingleIceCream = (req,res) => {
+    IceCream.updateOne({_id:req.params._id},
+        req.body)
+        .then(singleIceCream => res.json({results: singleIceCream}))
+        .catch(err => res.json({message: "that didn't quite work", err}))
+}
+
+module.exports.addTopping = (req,res) => {
+
+    IceCream.updateOne({_id: req.params._id},
+        {$push: {toppings: req.body.topping}})
+        .then(singleIceCream => res.json({results: singleIceCream}))
+        .catch(err => res.json({message: "that didn't quite work", err}))
 }
--- backEnd/mongooseIntro/server/routes/iceCream.routes.js
+++ backEnd/mongooseIntro/server/routes/iceCream.routes.js
@@ -1,5 +1,11 @@
 const IceCreamController = require("../controllers/iceCream.controller");
+const IceCream = require("../models/iceCream.model");
 
 module.exports = app => {
-    app.get("/api/allIceCreams", IceCreamController.findAllIceCreams);
+    app.get("/api/iceCreams/all", IceCreamController.findAllIceCreams);
+    app.post("/api/iceCreams/create", IceCreamController.createIceCream);
+    app.get("/api/iceCreams/:_id", IceCreamController.findSingleIceCream);
+    app.delete("/api/iceCreams/:_id/delete", IceCreamController.deleteSingleIceCream);
+    app.patch("/api/iceCreams/:_id/update", IceCreamController.updateSingleIceCream);
+    app.put("/api/iceCreams/:_id/addTopping", IceCreamController.addTopping);
 }
